df0688e19b0f434e3fa697bc3c19112875ef9c87
thimiranirmal <nirmalthimira@gmail.com>
2021-06-18 21:24:59 +0530

    Login auth

---
+++ Blog/app/Http/Controllers/AdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Admin;
+
+class AdminController extends Controller
+{
+    //login
+    public function login(){
+        return view('login');
+    }
+    //submit_login
+    public function submit_login(Request $request){
+        $request->validate([
+            'email'=>'required',
+            'password'=>'required',
+        ]);
+        $usercheck=Admin::where(['email'=>$request->email,'password'=>$request->password])->count();
+        if($usercheck>0){
+            return redirect('admin/dashboard');
+        }
+        else{
+            return redirect('admin/login')->with('error','Invalid Login');
+        }
+
+        
+    }
+    //dasboard
+    public function dashboard(){
+        return view('dashboard');
+    }
+}
